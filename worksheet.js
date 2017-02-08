60ca8feaad86bb85355f0807a9333eb20b4fb56c
Taylor Lang <lang.taylor.j@gmail.com>
2017-02-07 18:30:00 -0600

    steps 0-5ish complete thus far

--- worksheet.js
+++ worksheet.js
@@ -3,12 +3,28 @@
  *
  * Write a function that calculates the sum of all the numbers in an array
  */
-
+var arraySum = function(x) {
+    var sum = 0
+	for (var i = 0; i < x.length; i++) {
+        sum += x[i]
+    }
+    return sum
+}
 // PART 1
 
 // Write a function maxOfArray() that takes an array of
 // numbers as an argument and returns the highest number in the array.
 
+var maxOfArray = function(numberArray) {
+	var max = 0
+	for(var i = 0; i < numberArray.length; i++) {
+        log(numberArray[i])
+		if (numberArray[i] > max)	{
+			max = numberArray[i]
+		}
+    }
+	return max
+}
 
 /**
  * PART 2
@@ -17,6 +33,17 @@
  * as input and returns true if it is a vowel, false otherwise.
  */
 
+var isVowel = function(char) {
+    var vowelArray = ['a','e','i','o','u']
+ 	for (var i = 0; i<vowelArray.length; i=i+1) {
+        if (char === vowelArray[i]) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
 
 /**
  * Part 3
@@ -26,6 +53,14 @@
  * reverse("skoob") should return the
  * string "books".
  */
+
+var reverse = function(word) {
+ var revWord = ''
+ for (var i = word.length-1; i>=0; i--) {
+ revWord += word[i] 
+ }
+  return revWord
+ }
 
 
 /**
@@ -41,7 +76,24 @@
  For example, the fizzbuzz string for the number 3 is "..fizz"
  For the number 15, the fizzbuzz string is "..fizz.buzzfizz..fizzbuzz.fizz..fizzbuzz"
  */
-
+var fizzy = function(fizznum) {
+    var fizzstring = ''
+    //if ( var i = 0; /*//figuring out how to apply all numbers*/; i++;) {
+    if (i !== fizznum % 3 && i !== fizznum % 5) {
+      return '.'   
+     }
+    else if (i === fizznum % 3 && i !== fizznum % 5) {
+         return "fizz"   
+        }
+    else if (i !== fizznum % 3 && i === fizznum % 5) {
+         return "buzz"
+    	}
+    else if (i === fizznum % 3 && i === fizznum % 5) {
+     	 return "fizzbuzz"   
+    	}
+    }
+        
+}
 
 /**
  * Part 5
